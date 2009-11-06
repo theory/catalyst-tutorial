7498d1105d556cd1055943e9f6cd1b48bb3dd3ee
David E. Wheeler <david@justatheory.com>
2009-11-05 22:44:45 -0800

    Add Books controller.

---
+++ lib/MyApp/Controller/Books.pm
@@ -0,0 +1,42 @@
+package MyApp::Controller::Books;
+
+use strict;
+use warnings;
+use parent 'Catalyst::Controller';
+
+=head1 NAME
+
+MyApp::Controller::Books - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched MyApp::Controller::Books in Books.');
+}
+
+
+=head1 AUTHOR
+
+David E. Wheeler
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
