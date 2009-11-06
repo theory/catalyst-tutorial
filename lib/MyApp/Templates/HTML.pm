5df27154ade7425d3f44a51afc91e22af94cc28a
David E. Wheeler <david@justatheory.com>
2009-11-05 22:57:32 -0800

    Add wrapper template.

--- lib/MyApp/Templates/HTML.pm
+++ lib/MyApp/Templates/HTML.pm
@@ -4,16 +4,74 @@ use strict;
 use warnings;
 use parent 'Template::Declare::Catalyst';
 use Template::Declare::Tags;
+use Sub::Exporter -setup => { exports => [qw(wrapper) ] };
 
-# See Template::Declare docs for details on creating templates, which look
-# something like this.
-# template hello => sub {
-#     my ($self, $vars) = @_;
-#     html {
-#         head { title { "Hello, $vars->{user}" } };
-#         body { h1    { "Hello, $vars->{user}" } };
-#     };
-# };
+create_wrapper wrapper => sub {
+    my ($code, $c, $args) = @_;
+    xml_decl { 'xml', version => '1.0' };
+    outs_raw '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" '
+           . '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+    html {
+        head {
+            title { $args->{title} || 'My Catalyst App!' };
+            link {
+                rel is 'stylesheet';
+                href is $c->uri_for('/static/css/main.css' );
+            };
+
+        };
+
+        body {
+            div {
+                id is 'header';
+                # Your logo can go here.
+                img {
+                    src is $c->uri_for('/static/images/btn_88x31_powered.png');
+                };
+                # Page title.
+                h1 { $args->{title} || $c->config->{name} };
+            }; # end header.
+
+            div {
+                id is 'bodyblock';
+                div {
+                    id is 'menu';
+                    h3 { 'Navigation' };
+                    ul {
+                        li {
+                            a {
+                                href is $c->uri_for('/books/list');
+                                'Home';
+                            };
+                        };
+                        li {
+                            a {
+                                href is $c->uri_for('/');
+                                title is 'Catalyst Welcome Page';
+                                'Welcome';
+                            };
+                        };
+                    };
+                }; # end menu
+
+                div {
+                    id is 'content';
+                    # Status and error messages.
+                    if (my $msg = $args->{status_msg}) {
+                        span { class is 'message'; $msg };
+                    }
+                    if (my $err = $args->{error_msg}) {
+                        span { class is 'error'; $err };
+                    }
+
+                    # Output the template contents.
+                    $code->($args);
+                }; # end content
+
+            }; # end bodyblock
+        };
+    };
+};
 
 =head1 NAME
 
